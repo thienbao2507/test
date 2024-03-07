3a87a2e31375acb12e4e5e2812982ddc6dbb5cdb
thienbao2507 <truongphanthienbao2507@gmail.com>
2024-03-07 22:10:07 +0700

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Chúc cậu 8/3 vui vẻ",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Will u be my girl !!! </br></br></br>Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+  buttonYes: "Em đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn em.",
+  descriptionModar:
+    "Hãy cùng tận hưởng ngày của phụ nữ, ngày của phái đẹp. Hôm nay, mình chúc cho Nu sẽ là cô gái xinh đẹp nhất.</br> Love you <3",
+};
